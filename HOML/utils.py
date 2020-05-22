03ff391f1b0801aa3d95a0a6b2332515f7381e1b
Termanteus <bomcon123456@gmail.com>
2020-05-22 09:13:43 +0000

    Update utils v2

--- HOML/utils.py
+++ HOML/utils.py
@@ -112,3 +112,18 @@ def tensor_to_image(tensor):
       assert tensor.shape[0] == 1
       tensor = tensor[0]
     return PIL.Image.fromarray(tensor)
+
+def plot_learning_curves(loss, val_loss):
+    """
+        Examples:
+        plot_learning_curves(history.history["loss"], history.history["val_loss"])
+        plt.show()
+    """
+    plt.plot(np.arange(len(loss)) + 0.5, loss, "b.-", label="Training loss")
+    plt.plot(np.arange(len(val_loss)) + 1, val_loss, "r.-", label="Validation loss")
+    plt.gca().xaxis.set_major_locator(mpl.ticker.MaxNLocator(integer=True))
+    plt.axis([1, 20, 0, 0.05])
+    plt.legend(fontsize=14)
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.grid(True)
